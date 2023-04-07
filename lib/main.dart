7c0dab216ab719683a22f458e96f736e1e9fca3e
Brice G <bricegoudalo@gmail.com>
2023-04-07 23:53:12 +0200

    import routes/pages to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/common/routes/pages.dart';
 import 'package:chat_app/firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -22,6 +23,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      initialRoute: AppPages.INITIAL,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
